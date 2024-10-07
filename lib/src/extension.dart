97b3e3610c205b2cb6859b6595d6802a516eea07
Suebersson <suebersson.dev@gmail.com>
2024-10-07 17:40:46 -0300

    convert_json

--- lib/src/extension.dart
+++ lib/src/extension.dart
@@ -2,11 +2,25 @@ import './convert_json.dart';
 
 /// Auxiliar para converter objetos [Map] em json
 extension ConvertJsonForMap on Map<String, dynamic> {
+  /// Tipos de objetos padrão para o formato json Key:[String], Key:[int], Key:[bool] ou Key:[Null]
+  /// 
+  /// Tipos de objetos que podem ser codificados [Map<String, dynamic>], [Map<String, Object>],
+  /// [Map<String, String>], [List<Map<String, dynamic>>], [List<String|int|double|bool>], ...
+  ///
+  /// É uma [Map] ou uma [List] com instâncias de [Object], essa função deve ser usada com cuidado 
+  /// e não dispensa o uso de tratamentos de erro onde ela será usada
   String get encode => ConvertJson.encode(this);
 }
 
 /// Auxiliar para converter objetos [List] em json
 extension ConvertJsonForLit on List<dynamic> {
+  /// Tipos de objetos padrão para o formato json Key:[String], Key:[int], Key:[bool] ou Key:[Null]
+  /// 
+  /// Tipos de objetos que podem ser codificados [Map<String, dynamic>], [Map<String, Object>],
+  /// [Map<String, String>], [List<Map<String, dynamic>>], [List<String|int|double|bool>], ...
+  ///
+  /// É uma [Map] ou uma [List] com instâncias de [Object], essa função deve ser usada com cuidado 
+  /// e não dispensa o uso de tratamentos de erro onde ela será usada
   String get encode => ConvertJson.encode(this);
 }
 
